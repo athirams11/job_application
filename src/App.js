ff521d9ba933dc581b5de30a1a0f8c0242945ad2
athiramstrenser <athira.murali@trenser.com>
2022-08-27 21:13:10 +0530

    Remove unwanted code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Success from "./components/Success/success";
-import "./App.css";
 
 function App() {
   return (
